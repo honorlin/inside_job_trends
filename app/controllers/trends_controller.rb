d55c1e818d4849b757254f758eb58916f59e0eb2
Honor Lin <tenhon.lin@gmail.com>
2014-09-20 02:03:41 +0800

    add keyword fillter

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,9 +1,15 @@
 class TrendsController < ApplicationController
+
+	IGNORE_KEYWORD= [ "茶", "性" ]
+
   def index
 
+ 		@inside_jobs = InsideJob
 
-  	@inside_jobs = InsideJob.where.not("company LIKE ? OR company=''", "%茶%").order("post_date desc")
-		#.not({ :company => "" })
+		IGNORE_KEYWORD.each do |keyword|
+			@inside_jobs = @inside_jobs.ignore_keyword(keyword)
+		end
+
   end
 end
 
--- app/models/inside_job.rb
+++ app/models/inside_job.rb
@@ -1,2 +1,7 @@
 class InsideJob < ActiveRecord::Base
+	default_scope { order(:post_date => :desc) }
+
+	def self.ignore_keyword(keyword)
+		where.not("company LIKE ?", "%#{keyword}%")
+	end
 end
