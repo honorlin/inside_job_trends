698b86cd5b4cdde5764e22c2c4d1bf8927fb1d5e
Honor Lin <tenhon.lin@gmail.com>
2014-10-18 15:47:57 +0800

    analysis_service.rb

--- app/services/analysis_service.rb
+++ app/services/analysis_service.rb
@@ -2,7 +2,7 @@ class AnalysisService
 
 
 	def self.perform
-		AnalysisService::get_inside_all_jobs_url(3)
+		AnalysisService::get_inside_all_jobs_url(10)
   	AnalysisService::get_all_inside_jobs_infomation
 	end
 
@@ -11,16 +11,16 @@ private
 
   def self.get_inside_all_jobs_url(pages)
   	1.upto(pages.to_i) do |page|
-  		@inside_job_url_parser_service = InsideJobUrlParserService.new("http://jobs.inside.com.tw/jobs/page/#{page}")
-  		@inside_job_url_parser_service.parse
+  		@inside_job_url_parser_service = InsideJobUrlParserWithFilter.new("http://jobs.inside.com.tw/jobs/page/#{page}")
+  		@inside_job_url_parser_service.call
 		end 
   end
 
   def self.get_all_inside_jobs_infomation
   	@inside_jobs = Job.select(:url).where({ :title => nil })
   	@inside_jobs.each do |inside_job|
-			inside_job_parser_serice = InsideJobParserService.new(inside_job.url)
-  		inside_job_parser_serice.parse
+			inside_job_parser_serice = InsideJobParser.new(inside_job.url)
+  		inside_job_parser_serice.call
     end
   end
 
