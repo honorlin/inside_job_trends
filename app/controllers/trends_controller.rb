e2463a34dc3d70a01ef302475c6e9b2e06ea7e16
Honor Lin <tenhon.lin@gmail.com>
2014-10-18 22:59:25 +0800

    Panel show job class or program language count

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,5 +1,23 @@
 class TrendsController < ApplicationController
 
+	PANEL_SHOW = {
+
+		"前端工程師" => [ "前端" ],
+		"後端工程師" => [ "後端" ],
+		"Ruby on Rails" => [ "rails" ],
+		"NodeJs" => [ "nodejs" ],
+		"Python" => [ "python" ],
+		"PHP" => [ "php" ],
+		".NET" => [ ".net" ],
+		"Java" => [ "java" ],
+		"iOS" => [ "ios" ],
+		"Android" => [ "android" ],
+		"JaveScript" => [ "JaveScript" ],
+		"外商" => [ "外商" ],
+		"美商" => [ "美商" ]
+	}
+
+
   def index
 
  		@jobs = Job.in_effect
@@ -10,6 +28,15 @@ class TrendsController < ApplicationController
 
 		@in_effect_counts = @jobs.count
 
+		@shows = {}
+	
+		PANEL_SHOW.each do |key, value|
+			@shows[key] = Job.get_keywords_count(value)
+		end
+
+
+
+		Rails.logger.info @shows.to_s
 
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -9,5 +9,15 @@ class Job < ActiveRecord::Base
 		where("effective_date > ?", Time.now)
 	end
 
+	def self.get_keywords_count(keywords)
+			job = in_effect
+			keywords.each { |keyword| job = job.with_contains_keyword(keyword) }
+			job.count
+	end
+
+	def self.with_contains_keyword(keyword)
+		where("lower(job_info) LIKE ?", "%#{keyword.downcase}%")
+	end
+
 
 end
