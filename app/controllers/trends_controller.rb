b130fe23a07ce30ce8d10e85b3f9f89238b7a08c
Honor Lin <tenhon.lin@gmail.com>
2014-10-18 15:37:58 +0800

    use filter keywords yml

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,12 +1,10 @@
 class TrendsController < ApplicationController
 
-	IGNORE_KEYWORD= [ "茶", "性" , "男", "女", "小三", "LINE", "妹", "全套", "Make Love", "夜", "情", "姐", "妹", "0", "約", "送" ]
-
   def index
 
  		@jobs = Job.in_effect
 
-		IGNORE_KEYWORD.each do |keyword|
+		Settings.filter_keywords.each do |keyword|
 			@jobs = @jobs.ignore_company_keyword(keyword)
 		end
 
@@ -14,6 +12,8 @@ class TrendsController < ApplicationController
 
 
   end
+
+
 end
 
 
