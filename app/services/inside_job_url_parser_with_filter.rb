321240e858159aadfb97c82c0f6202962413f1c2
Honor Lin <tenhon.lin@gmail.com>
2014-10-18 15:47:31 +0800

    inside_job_url_parser_with_filter

---
+++ app/services/inside_job_url_parser_with_filter.rb
@@ -0,0 +1,31 @@
+require 'open-uri'
+require 'nokogiri'
+
+class InsideJobUrlParserWithFilter
+
+	def initialize(url)
+		@url = url
+	end
+
+	def call
+		html = Nokogiri::HTML(open(@url))
+
+		html.css('li.job').each do |job|
+			company_name = job.css(".role h3").text	
+			job_url = job.css("a")[0]["href"]
+
+			unless is_filted(company_name)
+				Job.create({ :url => job_url}) if !Job.find_by({ :url => job_url })
+			end
+
+		end
+
+	end
+
+private
+
+	def is_filted(company_name)
+		Settings.filter_keywords.any? { |keyword| company_name.include? keyword }
+	end
+
+end
